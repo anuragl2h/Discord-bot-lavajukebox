1b4b77789f57251c8c25003528fe9544fbeef2a4
anuragl2h <anuragtiwarigkp777@gmail.com>
2025-08-20 16:57:17 +0530

    Update st.py

--- st.py
+++ st.py
@@ -16,4 +16,4 @@ def handle_message(msg):
     send(msg, broadcast=True)  # broadcast to everyone
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True)
+    socketio.run(app,host="0.0.0.0")
