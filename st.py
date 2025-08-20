e03cb34619f6f23d8e4ee80f06bdc9c5292cc959
anuragl2h <anuragtiwarigkp777@gmail.com>
2025-08-20 17:40:08 +0530

    Update st.py

--- st.py
+++ st.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, send
+import os  # needed to get PORT from environment
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret!"  # needed for socketio
-socketio = SocketIO(app)
+socketio = SocketIO(app, async_mode="eventlet")  # async_mode required for Vercel
 
 @app.route("/")
 def index():
@@ -16,4 +17,5 @@ def handle_message(msg):
     send(msg, broadcast=True)  # broadcast to everyone
 
 if __name__ == "__main__":
-    socketio.run(app,host="0.0.0.0",port=port)
+    port = int(os.environ.get("PORT", 5000))  # get port from environment
+    socketio.run(app, host="0.0.0.0", port=port)
